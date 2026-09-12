41e68764b4689e30d7e2b9d39ac329337efd84e9
amruthesh-upfinzo <amurtheshcheral@upfinzo.in>
2026-09-12 20:20:44 +0530

    ProvenFoundationSection and ConnectedCore

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import AboutSection from "@/components/Home/AboutSection";
+import ConnectedCore from "@/components/Home/ConnectedCore";
 import CTASection from "@/components/Home/CTASection";
 import Hero from "@/components/Home/Hero";
+import ProvenFoundationSection from "@/components/Home/ProvenFoundationSection";
 import ServicesSection from "@/components/Home/ServicesSection";
 import ProductStack from "@/components/Home/ProductStack";
 
@@ -9,9 +11,10 @@ export default function HomePage() {
     <>
       <Hero />
       <AboutSection />
+      <ProvenFoundationSection />
+      <ConnectedCore />
       <ServicesSection />
       <ProductStack/>
-      
       <CTASection />
     </>
   );
