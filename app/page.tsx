eba4e6c3a2c0dadbafa31a3c4776c757cb2fef87
Linto-Upfinzo <lintothomas@upfinzo.in>
2026-09-14 11:35:22 +0400

    linto homepage

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,8 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <AboutSection />
       <ProvenFoundationSection />
       <ConnectedCore />
-      <ServicesSection />
       <ProductStack/>
       <CTASection />
     </>
